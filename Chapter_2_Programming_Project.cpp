80d7c29072e87e06c55f52f279855811f284a38c
Joshua Brunson <joshuarbrunson@gmail.com>
2025-02-25 21:11:39 -0500

    Problem 3 Complete

--- Chapter_2_Programming_Project.cpp
+++ Chapter_2_Programming_Project.cpp
@@ -1,11 +1,20 @@
 // Chapter_2_Programming_Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* This programs approximates the number of people that drinks energy drinks.
+I also approximates the number of people that prefer citrus flavored drinks*/
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int surveyValue = 16500; //assigns survey value
+	int energyDrinks = surveyValue * 0.15; //calculates the approximate number of people that drink one or more energy drinks a week
+	int citrusDrinks = energyDrinks * 0.58; //calculates the approximate number of people that drink citrus drinks
+
+	cout << "The approximate number of customers purchasing one or more energy drinks a week is " << energyDrinks << '.' << endl; // prints the engergy drinks value
+	cout << "The approximate number of customers purchasign citrus-flavored energy drinks is " << citrusDrinks << '.' << endl; // prints the citrus drinks value
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
