7a81630363296d91ba4991853b427e143ac87dca
Joshua Brunson <joshuarbrunson@gmail.com>
2025-02-25 20:09:42 -0500

    Ocean Levels complete

--- Chapter_2_Programming_Project.cpp
+++ Chapter_2_Programming_Project.cpp
@@ -3,18 +3,16 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    double growthRate = 1.5; 
+    double fiveYearValue = growthRate * 5; // value of growth after 5 years
+    double sevenYearValue = growthRate * 7; // Value of growth after 7 years
+    double tenYearValue = growthRate * 10; // Valus of growth after 10 years
+
+    cout << "The ocean level in five years will be " << fiveYearValue << " millimeters higher than the current level." << endl;
+    cout << "The ocean level in seven years will be " << sevenYearValue << " millimeters higer than the current level." << endl;
+    cout << "The ocean level in ten years will be " << tenYearValue << " millimeters than the current level." << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
